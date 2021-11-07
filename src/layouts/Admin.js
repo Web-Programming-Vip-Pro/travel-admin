5180ef9083727d4d8c85f40b90f45733bf58dc72
Thanh Minh <thanhgamedev@gmail.com>
2021-11-07 12:39:17 +0700

    feat(dashboard-index): update chart

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,7 +1,3 @@
-import React from 'react'
-
-// components
-
 import AdminNavbar from '@/components/Shared/Navbars/AdminNavbar.js'
 import Sidebar from '@/components/Shared/Sidebar/Sidebar.js'
 import HeaderStats from '@/components/Shared/Headers/HeaderStats.js'
@@ -15,7 +11,7 @@ export default function Admin({ children }) {
         <AdminNavbar />
         {/* Header */}
         <HeaderStats />
-        <div className="px-4 md:px-10 mx-auto w-full -m-24">
+        <div className="w-full px-4 mx-auto -m-24 md:px-10">
           {children}
           <FooterAdmin />
         </div>
