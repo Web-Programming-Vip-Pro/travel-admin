a10021905066389acd0ad946a99c38bbe359aea3
Thanh Minh <thanhgamedev@gmail.com>
2021-11-25 20:46:41 +0700

    feat(city): add search

--- src/pages/dashboard/cities.js
+++ src/pages/dashboard/cities.js
@@ -13,6 +13,7 @@ import Modal from '@/components/Shared/Modal'
 import { deleteCity } from '@/services/city'
 import PaginationButton from '@/components/Shared/Pagination'
 import Table from '@/components/Shared/Table'
+import TableHeader from '@/components/Shared/Table/TableHeader'
 
 function CityModal({ isOpen, toggleModal, isEdit, editedCity }) {
   return (
@@ -42,9 +43,10 @@ function Paginate({ currentPage, limit, onPageChange }) {
 }
 
 const Cities = () => {
-  const limit = 10
+  const [limit, setLimit] = useState(10)
   const [page, setPage] = useState(0)
-  const { cities, isLoading, error } = useCities(page, limit)
+  const [searchText, setSearchText] = useState('')
+  const { cities, isLoading, error } = useCities(page, limit, searchText)
   const [isModalOpen, toggleModal] = useToggle(false)
   const [city, setCity] = useState(null)
   const [isEdit, setEdit] = useState(false)
@@ -74,21 +76,21 @@ const Cities = () => {
 
   useEffect(() => {
     if (!isModalOpen) {
-      mutateCities(page, limit)
+      mutateCities(page, limit, searchText)
       mutateTotalCities(limit)
       setCity(null)
     }
   }, [isModalOpen])
 
   if (error) return <div>failed to load data</div>
-  if (isLoading) return <div>loading...</div>
   return (
     <>
       <div>
-        <button className="btn btn-success" onClick={handleAdd}>
-          <i className="fas fa-plus mr-2"></i>
-          Add
-        </button>
+        <TableHeader
+          toggleModal={handleAdd}
+          setLimit={setLimit}
+          setSearchText={setSearchText}
+        />
         {isModalOpen && (
           <CityModal
             isOpen={isModalOpen}
@@ -98,49 +100,54 @@ const Cities = () => {
           />
         )}
       </div>
-      <Table>
-        <Table.Head>
-          <th></th>
-          <th>Name</th>
-          <th>Country</th>
-          <th>Total Place</th>
-          <th>Description</th>
-          <th>Created At</th>
-          <th>Updated At</th>
-        </Table.Head>
-        <Table.Body>
-          {cities.map((city) => (
-            <tr key={city.id}>
-              <Table.Row>
-                <img
-                  src={city.image_cover}
-                  alt={city.name}
-                  className="w-12 h-12 rounded-full"
-                />
-              </Table.Row>
-              <Table.Row>{city.name}</Table.Row>
-              <Table.Row>{city.country.name}</Table.Row>
-              <Table.Row>{city.total_places}</Table.Row>
-              <Table.Row>{city.description}</Table.Row>
-              <Table.Row>{formatDate(city.created_at)}</Table.Row>
-              <Table.Row>{formatDate(city.updated_at)}</Table.Row>
-              <Table.Row>
-                <Table.EditButton onClick={() => handleEdit(city)} />
-              </Table.Row>
-              <Table.Row>
-                <Table.DeleteButton onClick={() => handleDelete(city.id)} />
-              </Table.Row>
-            </tr>
-          ))}
-        </Table.Body>
-      </Table>
-      <div>
-        <Paginate
-          currentPage={page}
-          limit={limit}
-          onPageChange={(page) => setPage(page)}
-        />
-      </div>
+      {isLoading && <div>loading...</div>}
+      {!isLoading && cities && (
+        <>
+          <Table>
+            <Table.Head>
+              <th></th>
+              <th>Name</th>
+              <th>Country</th>
+              <th>Total Place</th>
+              <th>Description</th>
+              <th>Created At</th>
+              <th>Updated At</th>
+            </Table.Head>
+            <Table.Body>
+              {cities.map((city) => (
+                <tr key={city.id}>
+                  <Table.Row>
+                    <img
+                      src={city.image_cover}
+                      alt={city.name}
+                      className="w-12 h-12 rounded-full"
+                    />
+                  </Table.Row>
+                  <Table.Row>{city.name}</Table.Row>
+                  <Table.Row>{city.country.name}</Table.Row>
+                  <Table.Row>{city.total_places}</Table.Row>
+                  <Table.Row>{city.description}</Table.Row>
+                  <Table.Row>{formatDate(city.created_at)}</Table.Row>
+                  <Table.Row>{formatDate(city.updated_at)}</Table.Row>
+                  <Table.Row>
+                    <Table.EditButton onClick={() => handleEdit(city)} />
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.DeleteButton onClick={() => handleDelete(city.id)} />
+                  </Table.Row>
+                </tr>
+              ))}
+            </Table.Body>
+          </Table>
+          <div>
+            <Paginate
+              currentPage={page}
+              limit={limit}
+              onPageChange={(page) => setPage(page)}
+            />
+          </div>
+        </>
+      )}
     </>
   )
 }
--- src/services/city.js
+++ src/services/city.js
@@ -3,9 +3,12 @@ import axios from 'axios'
 import useSWR, { mutate } from 'swr'
 const ENDPOINT = process.env.NEXT_PUBLIC_ENDPOINT
 
-export function useCities(page = 0, limit = 20) {
+export function useCities(page = 0, limit = 20, text = null) {
+  if (text === '') text = null
   const { data, error } = useSWR(
-    `${ENDPOINT}/cities?page=${page}&limit=${limit}`,
+    `${ENDPOINT}/cities?page=${page}&limit=${limit}${
+      text ? `&text=${text}` : ''
+    }`,
     fetcher
   )
   return {
@@ -72,8 +75,13 @@ export async function updateCity(id, city) {
   }
 }
 
-export function mutateCities(page = 0, limit = 20) {
-  mutate(`${ENDPOINT}/cities?page=${page}&limit=${limit}`)
+export function mutateCities(page = 0, limit = 20, text = null) {
+  if (text === '') text = null
+  mutate(
+    `${ENDPOINT}/cities?page=${page}&limit=${limit}${
+      text ? `&text=${text}` : ''
+    }`
+  )
 }
 export function mutateTotalCities(limit = 20) {
   mutate(`${ENDPOINT}/city/pages?limit=${limit}`)
