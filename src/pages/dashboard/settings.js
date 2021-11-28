89069fb5a8e13d5e478749dd9d38b821e8fa0b85
Thanh Minh <thanhgamedev@gmail.com>
2021-11-28 19:53:36 +0700

    fix(settings): check role

--- src/pages/dashboard/settings.js
+++ src/pages/dashboard/settings.js
@@ -3,9 +3,14 @@ import AccountSettings from '@/components/settings/AccountSettings'
 import PasswordSettings from '@/components/settings/PasswordSetings'
 import Admin from '@/layouts/Admin.js'
 import { useState } from 'react'
+import { useSession } from 'next-auth/react'
+import { isAdmin } from '@/utils'
 
 export default function Settings() {
-  const [setting, setSetting] = useState(0)
+  const [setting, setSetting] = useState(1)
+  const { data: session } = useSession()
+  const user = session && session.user
+  const isUserAdmin = isAdmin(user)
   return (
     <>
       <h1 className="text-2xl font-bold">Settings</h1>
@@ -13,7 +18,7 @@ export default function Settings() {
         className="select select-bordered select-primary"
         onChange={(e) => setSetting(parseInt(e.target.value))}
       >
-        <option value={0}>General</option>
+        {isUserAdmin && <option value={0}>General</option>}
         <option value={1}>Account</option>
         <option value={2}>Password</option>
       </select>
